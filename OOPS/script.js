788de54467c915d7fa60b30b879fcbe8998745a2
SanJay1577 <sanjayprashanth1997@gmail.com>
2024-03-07 09:57:31 +0530

    OOPS completed.

--- OOPS/script.js
+++ OOPS/script.js
@@ -32,3 +32,122 @@ toy1.buildToy();
 toy1.info();
 toy2.buildToy();
 toy2.info();
+
+//This keyword
+const Obj = {
+  land: "Property Land in GrandFather",
+  getLand: function () {
+    console.log(this.land);
+  },
+  innerObj: {
+    land: "Property owned by father",
+    getLand: function () {
+      console.log(this.land);
+    },
+    //Tagets only the global object
+    getDetails: () => {
+      console.log("Inner Obj1", this);
+    },
+    //Target Immediate Parent
+    getDeatailsin() {
+      const arrowFunc = () => {
+        console.log("Inner Obj2", this);
+      };
+      arrowFunc();
+    },
+  },
+};
+
+console.log(Obj);
+Obj.getLand();
+Obj.innerObj.getLand();
+console.log("Global window", this);
+Obj.innerObj.getDetails();
+Obj.innerObj.getDeatailsin();
+
+//constructor functions
+// Create an object {}
+function Product(name, price, type) {
+  //{} creates an object {product_name : "HP53" }
+  this.product_name = name;
+  this.product_price = price;
+  this.product_type = type;
+  this.getDetails = function () {
+    console.log(`
+    Product Name : ${this.product_name}
+    Product Price : ${this.product_price}
+    Product type : ${this.product_type}
+    `);
+  };
+}
+
+Product.prototype.getPName = function () {
+  console.log("Product Name : ", this.product_name);
+};
+
+const HpLaptop = new Product("HP53", 50000, "Electronics");
+console.log(HpLaptop);
+HpLaptop.getDetails();
+HpLaptop.getPName();
+
+//Class in JS
+//4 Pillars
+//Encapsulation (data, method)
+//Inheritance
+//Abstraction
+//PolyMorphism
+
+class Guvi {
+  //Encapsulation
+  constructor(batch, topic, mentor, time, task) {
+    // {}
+    //Polymorphism
+    this.batch = `FSD${batch}WD2`;
+    this.topic = topic;
+    this.mentor = mentor;
+    this.time = time;
+    this.task = task;
+  }
+  //Abstraction
+  getTaskPercentage() {
+    let taskpercentage = (this.task * 100) / 67;
+    console.log("Task percentage ", taskpercentage.toFixed(2));
+  }
+  getBatchDetails() {
+    console.log("The batch info is", this.batch);
+  }
+  getTodayTopic() {
+    console.log("The Topic info is", this.topic);
+  }
+  getOverALlDetails() {
+    console.log(`
+       Batch : ${this.batch}
+       Topic :  ${this.topic}
+       Mentor : ${this.mentor}
+    `);
+  }
+}
+
+const todaySession = new Guvi(57, "OOPS", "Sanjay", "8.10", 60);
+console.log(todaySession);
+todaySession.getTaskPercentage();
+
+//Inheritance
+class Student extends Guvi {
+  constructor(batch, topic, mentor, time, task, name, course) {
+    super(batch, topic, mentor, time, task);
+    this.student_name = name;
+    this.student_course = course;
+  }
+  getStudentInfo() {
+    console.log(`
+    StudentName : ${this.student_name}
+    StudentCourse : ${this.student_course}
+    `);
+  }
+}
+
+const cubert = new Student(57, "OOPS", "Sanjay", "8.10", 60, "Cubert", "MERN");
+console.log(cubert);
+cubert.getStudentInfo();
+cubert.getTodayTopic();
