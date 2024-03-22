419575d7a2287a00aed804d63a745b5805c54971
SanJay1577 <sanjayprashanth1997@gmail.com>
2024-03-22 10:20:49 +0530

    events completed

--- JavaScript_DOM/DOM_IN_Func/script.js
+++ JavaScript_DOM/DOM_IN_Func/script.js
@@ -42,3 +42,141 @@ getBtn.addEventListener("click", () => {
   };
   console.log(userData);
 });
+
+// //mouse events (mousedown, mouseleave, mouseup)
+// document.addEventListener("mouseover", (e) => {
+//   console.log("mouse hovered", e);
+// });
+
+// //key events (keypress, keydown, keyup)
+// document.addEventListener("keydown", (e) => {
+//   console.log("key trggerd = ", e.key);
+// });
+
+// //form events ("submit", "input" , "change")
+// const myForm = document.querySelector("form");
+// myForm.addEventListener("change", (e) => {
+//   e.preventDefault();
+//   console.log(e.target);
+// });
+
+// //window events("load", "resize")
+// window.addEventListener("resize", (e) => {
+//   console.log("window resized", e);
+// });
+
+//Setimeout and setInterval
+let timeoutCount = 1; //3
+function TimeoutFunc() {
+  const id = setTimeout(() => {
+    res_div.innerHTML = timeoutCount; //2
+    timeoutCount++;
+    TimeoutFunc();
+    if (timeoutCount > 10) {
+      clearTimeout(id);
+      res_div.innerHTML = "Time out over";
+    }
+  }, 1000);
+}
+
+let intervalCount = 1;
+function IntervalFunc() {
+  const id = setInterval(() => {
+    res_div.innerText = intervalCount;
+    intervalCount++;
+    if (intervalCount > 10) {
+      clearInterval(id);
+      res_div.innerText = "Interval Over";
+    }
+  }, 1000);
+}
+
+// const value = eval("5+6*5");
+// console.log(value);
+
+//setTimeout (funchandler, timeout in ms)
+
+// const timeoutId = setTimeout(() => {
+//   console.log("after timeout");
+// }, 1000);
+// console.log(timeoutId);
+//clearTimeout(timeoutId) //timeout wil be remove
+//setInterval (funchandler, interval in ms)
+// const intervelId = setInterval(() => {
+//   console.log("event done in interval");
+// }, 1000);
+// console.log(intervelId);
+// clearInterval(intervelId); interval will be removed
+
+//Logic for pagination
+// dynamically change the slice data
+
+const dummyData = [
+  { name: "sanjay1" },
+  { name: "prahsnth" },
+  { name: "prachi" },
+  { name: "vennila" },
+  { name: "selvam" },
+  { name: "sanjay2" },
+  { name: "prahsnth" },
+  { name: "prachi" },
+  { name: "vennila" },
+  { name: "selvam" },
+  { name: "sanjay3" },
+  { name: "prahsnth" },
+  { name: "prachi" },
+  { name: "vennila" },
+  { name: "selvam" },
+];
+
+console.log(dummyData);
+
+function createCard(data) {
+  data.slice(10, 15).map((val) => {
+    document.body.innerHTML += `
+    <h1>Name : ${val.name}</h1>
+    `;
+  });
+}
+
+createCard(dummyData);
+//memeory task for calculator
+const memoryBTN = document.createElement("button");
+memoryBTN.setAttribute("id", "mplus");
+memoryBTN.innerText = "M";
+
+const memoryClearBTN = document.createElement("button");
+memoryClearBTN.setAttribute("id", "mclear");
+memoryClearBTN.innerText = "MC";
+
+const memoryplus = document.createElement("button");
+memoryplus.setAttribute("id", "mplus");
+memoryplus.innerText = "M+";
+document.body.append(memoryBTN, memoryClearBTN, memoryplus);
+
+memoryBTN.addEventListener("click", () => {
+  localStorage.setItem("memory", 40);
+});
+memoryplus.addEventListener("click", () => {
+  const value = localStorage.getItem("memory");
+  console.log(value);
+});
+memoryClearBTN.addEventListener("click", () => {
+  localStorage.removeItem("memory");
+});
+
+//form
+const task_form = document.getElementById("task-form");
+
+task_form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const namevalue = document.querySelector(".n-name").value;
+  const batchvalue = document.querySelector(".n-batch").value;
+  const task_table = document.getElementById("task-table");
+  const row = task_table.insertRow();
+  const col1 = row.insertCell(0);
+  const col2 = row.insertCell(1);
+  col1.innerText = namevalue;
+  col2.innerText = batchvalue;
+  task_form.reset();
+});
