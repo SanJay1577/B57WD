ff5cadb4dfaad0c1be29e5d21b7b058f777b6203
SanJay1577 <sanjayprashanth1997@gmail.com>
2024-05-08 10:42:50 +0530

    MOngoDb complete

--- Database/mongo.js
+++ Database/mongo.js
@@ -43,6 +43,11 @@ db.posts.insertMany([
   },
 ]);
 // creating collection using insert command
+db.referal.insertOne({
+  name: "Abdul",
+  company: "Netflix",
+  designation: "Architect",
+});
 db.referal.insertMany([
   {
     name: "sanjay",
@@ -88,3 +93,96 @@ db.referal.find({ _id: ObjectId("6639a9f4178a30fc4c2dcbe4") });
 db.referal.findOne({ company: "Amazon" });
 //getting all the filtered data alone
 db.referal.find({ company: "Amazon" });
+
+//cursor methods
+db.referal.find().toArray();
+
+db.referal.find().forEach((ref) => {
+  print("Ref-Name: ", ref.name);
+});
+
+//aggregater methods
+//sorting
+//ascending
+db.posts.find().sort({ experience: 1 });
+//descending
+db.posts.find().sort({ experience: -1 });
+//limiting
+db.posts.find().sort({ experience: -1 }).limit(2);
+//offset
+db.posts.find().sort({ experience: -1 }).limit(2).skip(2);
+//count
+db.posts.find().count();
+//greater than
+db.posts.find({ experience: { $gt: 2 } });
+//greater than or equal
+db.posts.find({ experience: { $gte: 2 } });
+//lesser than
+db.posts.find({ experience: { $lt: 3 } });
+//lesser than or equal
+db.posts.find({ experience: { $lte: 3 } });
+//in between
+db.posts.find({ experience: { $gt: 1, $lt: 5 } });
+//not
+db.posts.find({ experience: { $not: { $gt: 1, $lt: 5 } } });
+//or
+db.posts.find({ $or: [{ experience: { $lt: 5 } }, { package: { $gt: 20 } }] });
+//and
+db.posts.find({ $and: [{ experience: 2 }, { package: 12 }] });
+
+//Aggregation lookup
+
+db.posts.aggregate([
+  {
+    $lookup: {
+      from: "referal",
+      localField: "companyName",
+      foreignField: "company",
+      as: "Related Referals",
+    },
+  },
+]);
+//Aggregate group
+db.posts.insertOne({
+  companyName: "Myntra",
+  role: "SDE",
+  package: 31,
+  experience: 10,
+  location: "bangalore",
+});
+//group and matahc aggregations
+db.posts.aggregate([
+  { $match: { role: "SDE" } },
+  { $group: { _id: "$companyName", totalPackages: { $sum: "$package" } } },
+]);
+
+//Crud approach
+db.posts.updateOne(
+  { _id: ObjectId("663afd94a0eca5dc9809aa6d") },
+  { $set: { package: 21 } }
+);
+//updateMany
+db.posts.deleteOne({ _id: ObjectId("663afd94a0eca5dc9809aa6d") });
+//deleteMany
+
+//Aggregate operations
+
+db.referal
+  .aggregate([
+    {
+      $group: {
+        _id: "$name",
+        duplicate: { $addToSet: "$_id" },
+        totalCount: { $sum: 1 },
+      },
+    },
+    {
+      $match: {
+        totalCount: { $gt: 1 },
+      },
+    },
+  ])
+  .forEach((doc) => {
+    doc.duplicate.shift();
+    db.referal.deleteMany({ _id: { $in: doc.duplicate } });
+  });
